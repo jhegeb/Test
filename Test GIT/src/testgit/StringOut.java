4527a31a1c94c7ee163533184d67543853f320b2
Patrik Karlsson <jhegeb@gmail.com>
2014-04-01 17:32:22 +0200

    First checkin

---
+++ Test GIT/src/testgit/StringOut.java
@@ -0,0 +1,15 @@
+package testgit;
+
+public class StringOut {
+	
+	public StringOut(){
+		
+	}
+	
+	public void hello(){
+		
+		System.out.println("Hello GIT!");
+		
+	}
+
+}
---
+++ Test GIT/src/testgit/TestMain.java
@@ -0,0 +1,15 @@
+package testgit;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringOut so = new StringOut();
+		so.hello();
+		
+
+
+	}
+
+}
